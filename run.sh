aaaa9e1edc557ca47e124e67d6a678187af9f646
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-02-10 13:10:53 +0000

    Updated scripts and readme.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ pip install -r requirements.txt
 
 export PYTHONPATH=$PYTHONPATH:.
 
-python parts_genie/client.py https://parts.synbiochem.co.uk/ data/sbol.xml 37762 out
+python parts_genie/client.py https://parts.synbiochem.co.uk/ data/sbol_rbs.xml 37762 out
